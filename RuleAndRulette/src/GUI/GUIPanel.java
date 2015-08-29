975ece1324904466039ded968c69104cb3d405b5
Hamish <LucidNeko@users.noreply.github.com>
2015-08-30 05:46:41 +1200

    Convert GUIPanel to blit to canvas

--- RuleAndRulette/src/GUI/GUIPanel.java
+++ RuleAndRulette/src/GUI/GUIPanel.java
@@ -65,38 +65,28 @@ public class GUIPanel extends JPanel {
 		super.paintComponent(gold);
 		Graphics2D g = (Graphics2D) gold;
 
-		// if( game == null ){
-		// return;
-		// }
-		//
-		// // I DRAW STUFF HERE
-		// List<Entity> entities = game.getEntities();
-		// if(entities == null ){
-		// return;
-		// }
-		//
-		// for(int i = 0; i < entities.size(); i++){
-		// entities.get(i).render(canvas);
-		// }
-
-		g.drawImage(canvas.asBufferedImage(), null, (int) (camera.x), (int) (camera.y));
-
-		drawUI(g);
+		//level
+		Graphics2D g2d = canvas.asBufferedImage().createGraphics();
+		g2d.setFont(new Font(R.fonts.kenpixel_mini_square.getName(), Font.PLAIN, 30));
+		g2d.setColor(Color.black);
+		g2d.drawString("Level: " + game.getCurrentLevel(), 38, 60);
+		g2d.dispose();
 		
 		if( game.getStatus() == Game.GAMESTATUS_WON ){
-			drawWonStatus(g);
+			drawWonStatus(canvas);
 		}
+		
+		g.drawImage(canvas.asBufferedImage(), null, (int) (camera.x), (int) (camera.y));
 	};
 
-	private void drawWonStatus(Graphics2D g) {
-		Point2D centerOfScreen = new Point2D.Double(((int)this.getWidth()/2), ((int)(this.getHeight()/2)));
+	private void drawWonStatus(PixelImage canvas) {
+		Graphics2D g = canvas.asBufferedImage().createGraphics();
+		
+		Point2D centerOfScreen = new Point2D.Double(canvas.getWidth()/2, canvas.getHeight()/2);  //new Point2D.Double(((int)this.getWidth()/2), ((int)(this.getHeight()/2)));
 		
 
 		BufferedImage heart = R.gui.heart.asBufferedImage();
 		g.drawImage(heart, ((int)centerOfScreen.getX()-heart.getWidth()/2), ((int)(centerOfScreen.getY()/2)-heart.getHeight()/2), null);
-		
-		BufferedImage success = R.gui.success.asBufferedImage();
-//		g.drawImage(success, ((int)centerOfScreen.getX()-success.getWidth()/2), ((int)centerOfScreen.getY()-success.getHeight()/2), null);
 		
 		g.setFont(new Font(R.fonts.kenpixel_mini_square.getName(), Font.PLAIN, 60));
 		g.setColor(Color.black);
@@ -104,64 +94,73 @@ public class GUIPanel extends JPanel {
 		int x = (int) (centerOfScreen.getX() - bounds.getWidth()/2);
 		int y = (int) (centerOfScreen.getY() + bounds.getHeight()/4);
 		g.drawString("Success", x, y);
-		
+		g.dispose();
 	}
 
-	private void drawUI(Graphics2D g) {
+//	private void drawUI(Graphics2D g) {
 		// Draw which level we are on
-		BufferedImage levelImage = R.gui.level.asBufferedImage();
-		g.drawImage(levelImage, null, 0, 0);
-		BufferedImage[] levelNumbers = getCurrentLevelImage();
-		int heightDiff = Math.abs(levelImage.getHeight() / 2
-				- levelNumbers[0].getHeight() / 2);
-		int y = levelImage.getMinY() + heightDiff;
-		for (int i = 0; i < levelNumbers.length; i++) {
-			g.drawImage(levelNumbers[i], null, levelImage.getMinX()
-					+ levelImage.getWidth() + (i * levelNumbers[0].getWidth()),
-					y);
-		}
-	}
+//		BufferedImage levelImage = R.gui.level.asBufferedImage();
+//		g.drawImage(levelImage, null, 0, 0);
+//		BufferedImage[] levelNumbers = getCurrentLevelImage();
+//		int heightDiff = Math.abs(levelImage.getHeight() / 2
+//				- levelNumbers[0].getHeight() / 2);
+//		int y = levelImage.getMinY() + heightDiff;
+//		for (int i = 0; i < levelNumbers.length; i++) {
+//			g.drawImage(levelNumbers[i], null, levelImage.getMinX()
+//					+ levelImage.getWidth() + (i * levelNumbers[0].getWidth()),
+//					y);
+//		}
+		
+//		String level = "Level: " + game.getCurrentLevel();
+//		
+//		g.setFont(new Font(R.fonts.kenpixel_mini_square.getName(), Font.PLAIN, 30));
+//		g.setColor(Color.black);
+//		Rectangle2D bounds = g.getFontMetrics().getStringBounds(level, g);
+////		int x = (int) (centerOfScreen.getX() - bounds.getWidth()/2);
+////		int y = (int) (centerOfScreen.getY() + bounds.getHeight()/4);
+//		g.drawString(level, 38, 60);
+//	}
 
-	private BufferedImage[] getCurrentLevelImage() {
-		String string = String.valueOf(game.getCurrentLevel());
-		int numbers = string.length();
-		BufferedImage[] images = new BufferedImage[numbers];
-		for (int i = 0; i < numbers; i++) {
-			int insideNumber = Integer
-					.parseInt(String.valueOf(string.charAt(i)));
-			switch (insideNumber) {
-			case 1:
-				images[i] = R.gui.one.asBufferedImage();
-				break;
-			case 2:
-				images[i] = R.gui.two.asBufferedImage();
-				break;
-			case 3:
-				images[i] = R.gui.three.asBufferedImage();
-				break;
-			case 4:
-				images[i] = R.gui.four.asBufferedImage();
-				break;
-			case 5:
-				images[i] = R.gui.five.asBufferedImage();
-				break;
-			case 6:
-				images[i] = R.gui.six.asBufferedImage();
-				break;
-			case 7:
-				images[i] = R.gui.seven.asBufferedImage();
-				break;
-			case 8:
-				images[i] = R.gui.eight.asBufferedImage();
-				break;
-			case 9:
-				images[i] = R.gui.nine.asBufferedImage();
-				break;
-			case 0:
-				images[i] = R.gui.zero.asBufferedImage();
-			}
-		}
-		return images;
-	}
+//	private BufferedImage[] getCurrentLevelImage() {
+//		String string = String.valueOf(game.getCurrentLevel());
+//		int numbers = string.length();
+//		BufferedImage[] images = new BufferedImage[numbers];
+//		for (int i = 0; i < numbers; i++) {
+//			int insideNumber = Integer
+//					.parseInt(String.valueOf(string.charAt(i)));
+//			switch (insideNumber) {
+//			case 1:
+//				images[i] = R.gui.one.asBufferedImage();
+//				break;
+//			case 2:
+//				images[i] = R.gui.two.asBufferedImage();
+//				break;
+//			case 3:
+//				images[i] = R.gui.three.asBufferedImage();
+//				break;
+//			case 4:
+//				images[i] = R.gui.four.asBufferedImage();
+//				break;
+//			case 5:
+//				images[i] = R.gui.five.asBufferedImage();
+//				break;
+//			case 6:
+//				images[i] = R.gui.six.asBufferedImage();
+//				break;
+//			case 7:
+//				images[i] = R.gui.seven.asBufferedImage();
+//				break;
+//			case 8:
+//				images[i] = R.gui.eight.asBufferedImage();
+//				break;
+//			case 9:
+//				images[i] = R.gui.nine.asBufferedImage();
+//				break;
+//			case 0:
+//				images[i] = R.gui.zero.asBufferedImage();
+//			}
+//		}
+//		return images;
+//	}
 
 }
